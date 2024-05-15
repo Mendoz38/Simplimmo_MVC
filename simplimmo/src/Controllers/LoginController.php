67fea61d3a9a1c2f6d6154e43ff9c297adf84b26
unknown <cedric@mendoz.fr>
2024-05-15 11:54:32 +0200

    login test

--- simplimmo/src/Controllers/LoginController.php
+++ simplimmo/src/Controllers/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../Services/Response.php';
+require_once __DIR__ . '/../Repositories/UserRepository.php';
 
 class LoginController
 {
@@ -8,10 +9,40 @@ class LoginController
 
     public function index()
     {
+        $loginRepository = new LoginRepository();
+        $userRepository = new UserRepository();
+
+        $successMessage = '';
+        $errMessage = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
+
+            if ($userRepository->checkEmailExists($mail)) {
+                $hashedPassword = $userRepository->checkPassword($mail);
+                
+                echo  "<br />password1".$password;
+               // echo  "<br />password2".$hashedPassword;
+
+                if (password_verify($password, $hashedPassword)) {
+                    echo 'Le mot de passe est valide !';
+                } else {
+                    echo 'Le mot de passe est invalide.';
+                }
+
+            } else {
+                echo'Utilisateur non trouvé';
+            }
+        }
+
+
         $title = "Login!";
 
         $viewData = [
-			'title' => $title
+			'title' => $title,
+            'successMessage' => $successMessage,
+            'errMessage' => $errMessage
         ];
 
         $this->render('LoginTemplate', $viewData);
